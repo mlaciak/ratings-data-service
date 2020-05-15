e4a7b54d37e3e80e9ad04d9a61a1583454fc4543
Michał Łaciak <laciak.michal@gmail.com>
2020-05-15 14:01:59 +0200

    Cloud config 3

--- src/main/java/com/example/ratingsdataservice/resources/RatingsDataResource.java
+++ src/main/java/com/example/ratingsdataservice/resources/RatingsDataResource.java
@@ -2,6 +2,7 @@ package com.example.ratingsdataservice.resources;
 
 import com.example.ratingsdataservice.models.Rating;
 import com.example.ratingsdataservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,12 @@ import java.util.List;
 @RequestMapping("/ratingsdata")
 public class RatingsDataResource {
 
+    @Value("${firstId}")
+    private String first;
+
+    @Value("${secondId}")
+    private String second;
+
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
@@ -21,9 +28,9 @@ public class RatingsDataResource {
     @RequestMapping("users/{userId}")
     public UserRating getUserRating(@PathVariable("userId") String userId) {
         List<Rating> ratings = Arrays.asList(
-                new Rating("123", 4),
-                new Rating("567", 3)
-        );
+                new Rating(first, 4),
+                new Rating(second, 3)
+        ); //123 567
         UserRating userRating = new UserRating();
         userRating.setUserRating(ratings);
         return userRating;
